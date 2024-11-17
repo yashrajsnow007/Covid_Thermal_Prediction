ac91382e6718fe486a5e64dc0e5789c9f1b4108e
Yashraj Chaturvedi <149673583+yashrajsnow007@users.noreply.github.com>
2024-11-18 01:57:56 +0530

    Update team.js

--- src/app/team.js
+++ src/app/team.js
@@ -3,44 +3,55 @@ export default function Team() {
     <div className="w-4/5">
       <div className="w-full flex flex-wrap justify-between gap-y-4">
         <div className="h-60 w-60 bg-slate-400 rounded-md hover:bg-slate-300">
-            <div className="h-2/3">
+          <div className="h-2/3">
             <img
-            src="/images/AG.jpg"
-            alt="Anushk Gupta"
-            className="rounded-md object-cover w-full h-full"
-            style={{ objectPosition: "50% 15%" }}
-          />
-            </div>
-          
+              src="/images/AG.jpg"
+              alt="Anushk Gupta"
+              className="rounded-md object-cover w-full h-full"
+              style={{ objectPosition: "50% 15%" }}
+            />
+          </div>
           <div className="flex flex-col items-center">
             <h1 className="text-1xl">Anushk Gupta</h1>
-            <p>Percpetron, ANN</p>
+            <p>Perceptron, ANN</p>
             <ul className="flex gap-2">
               <li>
-                <a href="https://github.com/introspective321/" target="_blank">
+                <a
+                  href="https://github.com/introspective321/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    src="https://cdn-icons-png.freepik.com/256/733/733609.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="G"
+                    src="https://cdn-icons-png.freepik.com/256/733/733609.png"
+                    alt="GitHub"
                     className="h-5 w-5"
-                  ></img>
+                  />
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/introspective321/" target="_blank">
+                <a
+                  href="https://www.linkedin.com/in/introspective321/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    src="https://cdn-icons-png.freepik.com/256/1384/1384014.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="L"
+                    src="https://cdn-icons-png.freepik.com/256/1384/1384014.png"
+                    alt="LinkedIn"
                     className="h-5 w-5"
-                  ></img>
+                  />
                 </a>
               </li>
               <li>
-                <a href="https://twitter.com/anushkg_84" target="_blank">
+                <a
+                  href="https://twitter.com/anushkg_84"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    src="https://cdn-icons-png.freepik.com/256/5969/5969020.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="G"
+                    src="https://cdn-icons-png.freepik.com/256/5969/5969020.png"
+                    alt="Twitter"
                     className="h-5 w-5"
-                  ></img>
+                  />
                 </a>
               </li>
             </ul>
@@ -48,181 +59,112 @@ export default function Team() {
         </div>
 
         <div className="h-60 w-60 bg-slate-400 rounded-md hover:bg-slate-300">
-        <div className="h-2/3">
+          <div className="h-2/3">
             <img
-            src="/images/YC.jpeg"
-            alt="Yashraj Chaturvedi"
-            className="rounded-md object-cover object-top w-full h-full"
-          />
-            </div>
+              src="/images/YC.jpeg"
+              alt="Yashraj Chaturvedi"
+              className="rounded-md object-cover object-top w-full h-full"
+            />
+          </div>
           <div className="flex flex-col items-center">
             <h1 className="text-1xl">Yashraj Chaturvedi</h1>
             <p>Decision Tree, SVM</p>
             <ul className="flex gap-2">
               <li>
-                <a href="https://github.com/yashrajsnow007" target="_blank">
+                <a
+                  href="https://github.com/yashrajsnow007"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    src="https://cdn-icons-png.freepik.com/256/733/733609.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="G"
+                    src="https://cdn-icons-png.freepik.com/256/733/733609.png"
+                    alt="GitHub"
                     className="h-5 w-5"
-                  ></img>
+                  />
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/yashraj-chaturvedi-b20b8a256/" target="_blank">
+                <a
+                  href="https://www.linkedin.com/in/yashraj-chaturvedi-b20b8a256/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    src="https://cdn-icons-png.freepik.com/256/1384/1384014.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="L"
+                    src="https://cdn-icons-png.freepik.com/256/1384/1384014.png"
+                    alt="LinkedIn"
                     className="h-5 w-5"
-                  ></img>
+                  />
                 </a>
               </li>
               <li>
-                <a href="https://twitter.com/YashrajChaturv6" target="_blank">
+                <a
+                  href="https://twitter.com/YashrajChaturv6"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    src="https://cdn-icons-png.freepik.com/256/5969/5969020.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="G"
+                    src="https://cdn-icons-png.freepik.com/256/5969/5969020.png"
+                    alt="Twitter"
                     className="h-5 w-5"
-                  ></img>
+                  />
                 </a>
               </li>
             </ul>
           </div>
         </div>
+
         <div className="h-60 w-60 bg-slate-400 rounded-md hover:bg-slate-300">
-        <div className="h-2/3">
+          <div className="h-2/3">
             <img
-            src="/images/AA.jpeg"
-            alt="Omprakash Nain"
-            className="rounded-md object-cover w-full h-full"
-          />
-            </div>
+              src="/images/AA.jpeg"
+              alt="Omprakash Nain"
+              className="rounded-md object-cover w-full h-full"
+            />
+          </div>
           <div className="flex flex-col items-center">
             <h1 className="text-1xl">Omprakash Nain</h1>
             <p>Naive Bayes, SVM</p>
             <ul className="flex gap-2">
               <li>
-                <a href="https://github.com/aatifahmad123" target="_blank">
+                <a
+                  href="https://github.com/aatifahmad123"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    src="https://cdn-icons-png.freepik.com/256/733/733609.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="G"
+                    src="https://cdn-icons-png.freepik.com/256/733/733609.png"
+                    alt="GitHub"
                     className="h-5 w-5"
-                  ></img>
+                  />
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/aatif-ahmad-2b8862282/" target="_blank">
+                <a
+                  href="https://www.linkedin.com/in/aatif-ahmad-2b8862282/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    src="https://cdn-icons-png.freepik.com/256/1384/1384014.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="L"
+                    src="https://cdn-icons-png.freepik.com/256/1384/1384014.png"
+                    alt="LinkedIn"
                     className="h-5 w-5"
-                  ></img>
+                  />
                 </a>
               </li>
               <li>
-                <a href="https://x.com/ByteKnight18?t=i1CfrQI3RYK01tx5_3sZXw&s=08" target="_blank">
+                <a
+                  href="https://x.com/ByteKnight18"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <img
-                    src="https://cdn-icons-png.freepik.com/256/5969/5969020.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="G"
+                    src="https://cdn-icons-png.freepik.com/256/5969/5969020.png"
+                    alt="Twitter"
                     className="h-5 w-5"
-                  ></img>
+                  />
                 </a>
               </li>
             </ul>
-          </div>
-        </div>
-
-        <div className="h-60 w-60 bg-slate-400 rounded-md hover:bg-slate-300">
-        <div className="h-2/3">
-            <img
-            src="/images/KA.jpg"
-            alt="Khushi Agarwal"
-            className="rounded-md object-cover w-full h-full object-top"
-          />
-            </div>
-          <div className="flex flex-col items-center">
-            <h1 className="text-1xl">Khushi Agrawal</h1>
-            <p>KNN, ANN</p>
-            <ul className="flex gap-2">
-              <li>
-                <a href="https://github.com/b22cs005" target="_blank">
-                  <img
-                    src="https://cdn-icons-png.freepik.com/256/733/733609.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="G"
-                    className="h-5 w-5"
-                  ></img>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/khushi-agrawal-364888256" target="_blank">
-                  <img
-                    src="https://cdn-icons-png.freepik.com/256/1384/1384014.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="L"
-                    className="h-5 w-5"
-                  ></img>
-                </a>
-              </li>
-              </ul>
-          </div>
-        </div>
-
-        <div className="h-60 w-60 bg-slate-400 rounded-md hover:bg-slate-300">
-        <div className="h-2/3">
-            <img
-            src="/images/IS.png"
-            alt="Ishita Sancheti"
-            className="rounded-md object-cover w-full h-full"
-          />
-            </div>
-          <div className="flex flex-col items-center">
-            <h1 className="text-1xl">Ishita Sancheti</h1>
-            <p>PCA, KMeans</p>
-            <ul className="flex gap-2">
-              <li>
-                <a href="https://github.com/ishitasancheti29" target="_blank">
-                  <img
-                    src="https://cdn-icons-png.freepik.com/256/733/733609.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="G"
-                    className="h-5 w-5"
-                  ></img>
-                </a>
-              </li>
-              </ul>
-          </div>
-        </div>
-        
-
-        <div className="h-60 w-60 bg-slate-400 rounded-md hover:bg-slate-300">
-        <div className="h-2/3">
-            <img
-            src="/images/AR.png"
-            alt="Aditi Rawat"
-            className="rounded-md object-cover w-full h-full object-top"
-          />
-            </div>
-          <div className="flex flex-col items-center">
-            <h1 className="text-1xl">Aditi Rawat</h1>
-            <p>Logistic Regression</p>
-            <ul className="flex gap-2">
-              <li>
-                <a href="https://github.com/Aditi571" target="_blank">
-                  <img
-                    src="https://cdn-icons-png.freepik.com/256/733/733609.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="G"
-                    className="h-5 w-5"
-                  ></img>
-                </a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/aditi-rawat-276701256?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank">
-                  <img
-                    src="https://cdn-icons-png.freepik.com/256/1384/1384014.png?ga=GA1.1.317981237.1711607053&semt=ais_hybrid"
-                    alt="L"
-                    className="h-5 w-5"
-                  ></img>
-                </a>
-              </li>
-              </ul>
           </div>
         </div>
       </div>
